bfd85f4f92a5fe0d8a1b7d137732166c85aa4010
Matheus Aveiro <matheusaveirofagundes@gmail.com>
2025-01-30 11:34:30 -0300

    🧱 ci: Estrutura inicial

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+
+const ErrorComponent = () => {
+  // Seu código de erro aqui
+  return <div>Ocorreu um erro.</div>;
+};
+
+export default ErrorComponent;
