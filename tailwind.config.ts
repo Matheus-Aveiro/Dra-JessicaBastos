c4fb6539691eb6d759eed06a4fc6812553e088af
Matheus Aveiro <matheusaveirofagundes@gmail.com>
2025-01-30 15:17:30 -0300

    🔧 chore: Ajustes para melhor utilização

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primaryColor: 'var(--primary-color)',
+                secondaryColor: 'var(--secondary-color)',
+                tertiaryColor: 'var(--tertiary-color)',
+
+                primaryButtonColor: 'var(--primary-button-color)',
+                secondaryButtorColor: 'var(--secondary-button-color)',
+
+                primaryBackgroundColor: 'var(--primary-background)',
+                secondaryBackgroundColor: 'var(--secondary-background)',
+
+                primaryTextColor: 'var(--primary-text-color)',
+                secondaryTextColor: 'var(--secondary-text-color)',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
